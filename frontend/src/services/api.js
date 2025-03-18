8397c4d54f91df0e4331f2e6118ba755038a661a
Harshit <harshitdutttyagi@gmail.com>
2025-03-18 23:40:13 +0530

    URL Support added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with base URL
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
