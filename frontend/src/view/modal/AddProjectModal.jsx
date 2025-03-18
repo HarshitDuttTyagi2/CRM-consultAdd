658e82ad9141ce083644d860e7f1afb91f2fa2fc
Harshit <harshitdutttyagi@gmail.com>
2025-03-18 23:14:35 +0530

    Add Project TeamService.js import

--- frontend/src/view/modal/AddProjectModal.jsx
+++ frontend/src/view/modal/AddProjectModal.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getAllTeams } from "../../services/teamService";
+import { getAllTeams } from "../../services/TeamService";
 import { createProject } from "../../services/projectService";
 
 const AddProjectModal = ({ isOpen, onClose, onAddProject, clientId }) => {
